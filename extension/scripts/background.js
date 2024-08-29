72477619ba63b96b899d6ea5b6bc97fcaad6271f
Alvaro Montoro <alvaromontoro@gmail.com>
2024-08-28 19:35:38 -0500

    Update extension icons
    - Update icons
    - Remove badge when active
    - Upgrade version to 1.0.1

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -2,9 +2,7 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.action === "readPaymentMethods") {
     chrome.tabs.query({active: true, currentWindow: true}, ([tab]) => {
       if (tab.id) {
-        chrome.action.setIcon({tabId: tab.id, path: '/images/icon-48-on.png'});
-        chrome.action.setBadgeText({tabId: tab.id, text: '$'});
-        chrome.action.setBadgeBackgroundColor({color: '#44cc44'});
+        chrome.action.setIcon({tabId: tab.id, path: '/images/icon-128-on.png'});
       }
     });
 
