302cdc84df45332c3543dd6fa8f07b1a8fc79602
Alvaro Montoro <alvaromontoro@gmail.com>
2024-08-28 19:57:29 -0500

    Add crowdfundly
    - Add crowdfundly to payment constants
    - Add logo in webp
    - Update styles to include logo
    - Update README
    - Upgrade version to 1.1.0

--- extension/popup/constants.js
+++ extension/popup/constants.js
@@ -1,22 +1,33 @@
+/**
+ * Supported payment methods in alphabetical order.
+ * 
+ * [id]: key to the payment method (single word between quotes, must match logo file name)
+ * name: payment method name to be displayed in the popup
+ * url: address template ([ID] is a placeholder for the username)
+ */
 export const SUPPORTED_METHODS = {
-  patreon: {
-    name: "Patreon",
-    url: "https://www.patreon.com/[ID]/membership"
-  },
-  paypal: {
-    name: "PayPal",
-    url: "https://paypal.me/[ID]"
-  },
-  venmo: {
-    name: "Venmo",
-    url: "https://venmo.com/u/[ID]"
-  },
   "buy-me-a-coffee": {
     name: "Buy me a coffee",
     url: "https://buymeacoffee.com/[ID]"
   },
+  "crowdfundly": {
+    name: "Crowdfundly",
+    url: "https://crowdfundly.com/[ID]"
+  },
   "ko-fi": {
     name: "Ko-fi",
     url: "https://ko-fi.com/[ID]"
+  },
+  "patreon": {
+    name: "Patreon",
+    url: "https://www.patreon.com/[ID]/membership"
+  },
+  "paypal": {
+    name: "PayPal",
+    url: "https://paypal.me/[ID]"
+  },
+  "venmo": {
+    name: "Venmo",
+    url: "https://venmo.com/u/[ID]"
   }
 };
